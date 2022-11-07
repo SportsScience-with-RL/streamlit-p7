bf7d2cf8468888673ddc2f24beeb3fa322f0e3b4
Raphael Lagarde <raf.lgd@hotmail.fr>
2022-11-07 14:31:31 +0100

    maj setup theme

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 mkdir -p ~/.streamlit/
 
-echo "[server]
+echo "[theme]
+base='light'
+backgroundColor='#f1e7e7'
+secondaryBackgroundColor='#D7D8D7'
+[server]
 headless = true
 port = $PORT
 enableCORS = false"  > ~/.streamlit/config.toml
